102882ff429792149d51b3e433a8868b9f0e57a2
RestorerBarisereden <restorerzorzor@gmail.com>
2024-04-23 17:42:00 +0100

    first commit

---
+++ converts.js
@@ -0,0 +1,23 @@
+function convertUnderScore(str) {
+    let strArray = str.split(/[-_]/)
+    
+    for (let index = 0; index < strArray.length; index++) {
+
+        if (index == 0) {
+            continue
+        }
+
+        let currentString = strArray[index]
+        let capChar = currentString[0].toUpperCase()
+        let strRest = currentString.substring(1)
+
+        strArray[index] = capChar + strRest
+    }
+
+    return strArray.join("")
+}
+
+
+
+console.log(convertUnderScore("the-stealth-warrior"));
+console.log(convertUnderScore("The-stealth-warrior"));
